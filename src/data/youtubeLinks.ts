14d64c7dc6e10dea13afda5c780a4952abfdc2cc
salahudheenര് <salahudheen9745@gmail.com>
2026-01-01 22:42:37 +0530

    Fix Arabic + Malayalam translationList across all Dhikr screens

--- src/data/youtubeLinks.ts
+++ src/data/youtubeLinks.ts
@@ -12,5 +12,5 @@ export const youtubeLinks: Record<YoutubeType, string> = {
   duaQabar: "https://youtu.be/1qwJyfT-W_0?si=6NWGmH6MFeLvDkY9",
   haddad: "https://youtu.be/udcpdWbV3lc?si=wwKUPui4bPccBcUN",
   asmaulHusna: "https://youtu.be/PGrirHLtTNU?si=qQga3PUWKUG2GS17",
-  manqus: "https://youtu.be/PGrirHLtTNU?si=qQga3PUWKUG2GS17",
+  manqus: "https://youtu.be/U8e-APdqysY?si=1J0ei6fpYxJB64gp",
 };
