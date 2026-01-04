a4a34eb14b09be26a2b6a965ffa6cf4b51d22d10
salahudheenര് <salahudheen9745@gmail.com>
2026-01-04 14:32:52 +0530

    Fix Arabic + Malayalam translationList across all Dhikr screens

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-community|@react-native-async-storage|react-native-.*)/)'
+  ],
 };
